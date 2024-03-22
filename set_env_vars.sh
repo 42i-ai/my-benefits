cfe01e050f0a97aaca13054588d2af510b8df1c1
Bonesso, Diego (ESI) <diego.bonesso@adp.com>
2024-03-22 14:53:32 -0300

    create bash env vars

---
+++ set_env_vars.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Set the first environment variable
+export ACCESS_ID="value1"
+
+# Set the second environment variable
+export SECRET_KEY="value2"
+
+echo "Environment variables created:"
+echo "FIRST_VAR=$ACCESS_ID"
+echo "SECOND_VAR=$SECRET_KEY"
