856ffbf3b27f48c958281cfb4e9f9177d1e71182
Diego Bonesso <pixelrama@pm.me>
2024-03-23 17:33:10 -0300

    add log to the application

--- my_benefits/train_model.py
+++ my_benefits/train_model.py
@@ -12,17 +12,22 @@ from typing import List
 PDF_NO_OCR_PATH = "./my_benefits/data/pdf-no-ocr-data"
 RAW_DIRECTORY = "./my_benefits/raw"
 
-logging.basicConfig(
-    level=logging.INFO, 
-    format='%(asctime)s - %(levelname)s - %(message)s'
-                  )
 
 
-logger = logging.getLogger('./my_benefits/example_logger')
+logger = logging.getLogger('debug_logger')
+logger.setLevel(logging.DEBUG)  
+file_handler = logging.FileHandler('./my_benefits/logs/train_model.log')
+file_handler.setLevel(logging.DEBUG)  
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+
 
 for file in read_files_from_directory(PDF_NO_OCR_PATH):
     fitz.Document = open_pdf_file(os.path.join(PDF_NO_OCR_PATH,file))
     pages: List[str] = extract_text_from_pdf_with_pymupdf(fitz.Document)
     write_pdf_pages_to_file(RAW_DIRECTORY, file.replace(".pdf", ".txt"), pages)
+    logger.debug(f'file processed {file}')
 
 
