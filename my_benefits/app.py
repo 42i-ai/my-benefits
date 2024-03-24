1914f5ce4aaae38f32294f75eb37e2b8cb6f4b55
Diego Bonesso <pixelrama@pm.me>
2024-03-24 08:05:07 -0300

    add word cloud visualization

--- my_benefits/app.py
+++ my_benefits/app.py
@@ -5,6 +5,7 @@ from matplotlib import pyplot as plt
 from streamlit_option_menu import option_menu
 from train_model import extract_pdf_text
 from train_model import write_corpus_to_file
+from train_model import get_list_of_words_from_document
 from wordcloud import WordCloud
 DEFAULT_HIGHLIGHT_PROBABILITY_MINIMUM = 0.001
 DEFAULT_NUM_TOPICS = 6
@@ -19,10 +20,11 @@ def extract_information_from_the_pdf_files():
 
 @st.experimental_memo()
 def generate_wordcloud():
-
-    wordcloud_text = (' '.join(' '.join(doc) for doc in docs))
+    
+    df = get_list_of_words_from_document()
+    wordcloud_text = ' '.join(df['Letter'].tolist())
     wordcloud = WordCloud(font_path=WORDCLOUD_FONT_PATH, width=700, height=600,
-                          background_color='white', collocations=collocations).generate(wordcloud_text)
+                          background_color='white', collocations=True).generate(wordcloud_text)
     return wordcloud
 
 st.set_page_config(
@@ -51,9 +53,19 @@ preprocessing_options = st.sidebar.form('preprocessing-options')
 
 with preprocessing_options:
         st.header('Extract data from PDF files')
-        submitted = st.form_submit_button("Extract Data")
+        submitted_extract_data = st.form_submit_button("Extract Data")
+
+visualization_options = st.sidebar.form('visualization-options')
+
+with visualization_options:
+        st.header('Visualize data')
+        submitted_visualize_data = st.form_submit_button("Visualize Data")
 
 
-if submitted:
+
+if submitted_extract_data:
     extract_information_from_the_pdf_files()
     st.write('Data has been extracted from the PDF files')
+
+if submitted_visualize_data:
+     generate_wordcloud()
