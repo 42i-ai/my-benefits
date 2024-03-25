4e762712501ccfcbdce55bd860b92f634fe325da
Diego Bonesso <pixelrama@pm.me>
2024-03-25 18:44:01 -0300

    generate wordcloud dashboard

--- my_benefits/app.py
+++ my_benefits/app.py
@@ -9,9 +9,10 @@ from document_model import DocumentsModel
 from topic_modeling import TopicModeling
 from extract_text_from_pdf_controller import ExtractTextFromPDFController
 from wordcloud import WordCloud
+from typing import List, Tuple
 
 
-WORDCLOUD_FONT_PATH = r'./assets/Inkfree.ttf'
+WORDCLOUD_FONT_PATH = "./my_benefits/assets/Inkfree.ttf"
 PDF_NO_OCR_PATH = "./my_benefits/data/pdf-no-ocr-data"
 
 RAW_DIRECTORY = "./my_benefits/raw"
@@ -42,6 +43,13 @@ if not os.path.exists(LOGS_DIRECTORY):
 document_model = DocumentsModel()
 extract_data_from_documents = ExtractTextFromPDFController()
 topic_modeling = TopicModeling()
+
+wordcloud_image = WordCloud(font_path=WORDCLOUD_FONT_PATH, width=700, height=600,
+                            background_color='white', collocations=True).generate("My benefits word cloud")
+
+li = [os.path.splitext(filename)[0]
+      for filename in os.listdir(PDF_NO_OCR_PATH)]
+li.insert(0, 'Select a file')
 
 
 def preprocessing_text() -> pl.DataFrame:
@@ -88,14 +96,45 @@ def extract_information_from_the_pdf_files():
         topic_modeling.train_model(preprocessed_documents)
 
 
-@st.cache_data()
-def generate_wordcloud():
+def generate_wordcloud(selected_event: str = None):
 
     # df = get_list_of_words_from_document()
     # wordcloud_text = ' '.join(df['Letter'].tolist())
     # wordcloud = WordCloud(font_path=WORDCLOUD_FONT_PATH, width=700, height=600,
     # background_color='white', collocations=True).generate(wordcloud_text)
-    return None  # wordcloud
+    if selected_event is None:
+        st.write('Please select a file')
+        return
+    if select_event == 'Select a file':
+        st.write('Please select a file')
+        return
+    with st.spinner(f'Generate word cloud for the document {select_event} ...'):
+        connection_silver = duckdb.connect(
+            document_model.get_silver_database_dir())
+        filename: str = selected_event + '.pdf'
+        # TODO: We can encapsulate this query in a method on the document_model class
+        document_tokens: pl.DataFrame = connection_silver.execute(
+            f"""
+                                    Select listagg(element)
+                                    from (
+                                        Select unnest(tokenized_text) AS element
+                                        from document_pages
+                                        where filename = '{filename}')
+                                        subquery
+                                    """).fetchall()[0][0].split(',')
+
+        topics_words = topic_modeling.get_list_of_topics_from_document(
+            document_tokens, MODELS_DIRECTORY)
+        wordcloud_list: List[str] = []
+        for words in topics_words:
+            wordcloud_list.append(words['Word'])
+        wordcloud_text = ' '.join(wordcloud_list)
+        wordcloud_image = WordCloud(font_path=WORDCLOUD_FONT_PATH, width=700, height=600,
+                                    background_color='white', collocations=True).generate(wordcloud_text)
+        plt.imshow(wordcloud_image, interpolation='bilinear')
+        plt.axis("off")
+        st.pyplot(plt)
+        st.write(f'Wordcloud for the document {filename}')
 
 
 st.set_page_config(
@@ -105,6 +144,11 @@ st.set_page_config(
     initial_sidebar_state="auto",
     menu_items=None)
 
+# with st.expander('Frequency Sized Corpus Wordcloud'):
+#    st.image(wordcloud_image.to_image(
+#    ), caption='Dataset Wordcloud (Not A Topic Model)', use_column_width=True)
+#    st.markdown(
+#        'These are the remaining words after document preprocessing.')
 
 preprocessing_options = st.sidebar.form('preprocessing-options')
 
@@ -116,6 +160,7 @@ visualization_options = st.sidebar.form('visualization-options')
 
 with visualization_options:
     st.header('Visualize data')
+    select_event = st.selectbox('Choisir des indicateurs', li)
     submitted_visualize_data = st.form_submit_button("Visualize Data")
 
 
@@ -124,4 +169,4 @@ if submitted_extract_data:
     st.write('Data has been extracted from the PDF files')
 
 if submitted_visualize_data:
-    generate_wordcloud()
+    wordcloud_image = generate_wordcloud(selected_event=select_event)
--- my_benefits/extract_text_from_pdf_controller.py
+++ my_benefits/extract_text_from_pdf_controller.py
@@ -65,3 +65,37 @@ class ExtractTextFromPDFController:
             self.logger.debug(
                 f"""Pages loaded from documents - {len(df)}""")
         return df
+
+    # TODO: Write test for this method
+    def process_pdf_file(self, pdf_file_path: str, pdf_filename: str) -> pl.DataFrame:
+        """
+        Extract text from each page from a PDF file and retun as a list of strings.
+        Parameters:
+        fitz.Documents: the result of the processing of the library pymupdf.
+        Returns:
+        list: a list of strings where each position is a page from the pdf.
+        """
+        schema = [
+            ("filename", pl.String),
+            ("page_number", pl.Int64),
+            ("text", pl.String)
+        ]
+        df = pl.DataFrame({name: pl.Series([], dtype=dtype)
+                          for name, dtype in schema})
+
+        document = fitz.open(os.path.join(pdf_file_path, pdf_filename))
+        self.logger.debug(
+            f"""Processing document - file documents - {pdf_filename}""")
+        page_number: int = 0
+        for page in document:
+            page_number += 1
+            row: Dictionary = {
+                "filename": pdf_filename,
+                "page_number": page_number,
+                "text": page.get_text().replace("\n", "  ")
+            }
+            row_df = pl.DataFrame([row])
+            df = pl.concat([df, row_df])
+        self.logger.debug(
+            f"""Pages loaded from documents - {len(df)}""")
+        return df
